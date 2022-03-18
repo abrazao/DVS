66d9aacce188ff61493f1ca37f992bcb3b48b6d7
abraz <abraz@192.168.0.8>
2022-03-18 12:25:40 -0300

    Primeiro Commit OO

---
+++ curso_programacao/src/curso_programacao/Quadrante.java
@@ -0,0 +1,21 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class Quadrante {
+
+	public static void main(String[] args) {
+		
+		double x, y;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o Valor de X:");
+		x = sc.nextDouble();
+		System.out.print("Digite o Valor de Y:");
+		y = sc.nextDouble();
+		
+		
+	}
+
+}
